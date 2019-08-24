ffecd92af11e647d837de9abccd62565ff5a4b28
Aileen Bartoletti <abartole@il-a2.21-school.ru>
2019-08-24 21:18:58 +0300

    Options Normed

--- inc/ft_ls.h
+++ inc/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: malannys <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/11 19:16:46 by malannys          #+#    #+#             */
-/*   Updated: 2019/08/24 20:46:29 by abartole         ###   ########.fr       */
+/*   Updated: 2019/08/24 21:16:47 by abartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,15 @@ typedef struct	s_node
 	struct s_node	*next;
 }				t_node;
 
-int		opt_parser(int ac, char **av, int *options); // options parser
+/*
+** Parsing options
+*/
+
+void	check_options(char *str, int *options);
+void	check_all_flags(int *options);
+int		opt_parser(int ac, char **av, int *options);
+
+
 
 void	read_dir(char *path, char *name, int *options);
 void	dir_recursive(t_node **head, int *options);
